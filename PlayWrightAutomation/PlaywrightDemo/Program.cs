8f683fa5f783a77bf7516ae11ad89380bf05a2e8
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-28 19:28:55 +0500

    5-28-2022

---
+++ PlayWrightAutomation/PlaywrightDemo/Program.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.Playwright;
+
+using var playwright = await Playwright.CreateAsync();
+await using var browser = await playwright.Chromium.LaunchAsync();
+var page = await browser.NewPageAsync();
+await page.GotoAsync("playwright.dev/dotnet/docs/intro");
+// await page.ScreenshotAsync(new PageScreenshotOptions { Path = "screenshot.png" });
